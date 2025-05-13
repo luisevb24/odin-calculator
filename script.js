f623d8bb9a2772a02f9d1b172eda5c0fc364d29c
luisevb24 <luisevb.5s@gmail.com>
2025-05-13 17:56:31 -0600

    complete logic of the calculator

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
-let aNumber = 0;
-let bNumber = 0;
-let operator = "x";
-const operators = ['+', '-', '*', '/'];
+let operationObj = {
+    a: '',
+    b: '',
+    operator: '',
+    operate(a, b, operator){
+        operator = this.operator;
+        a = Number(this.a);
+        b = Number(this.b);
+        switch(operator){
+            case '+':
+                this.result = add(a, b);
+                break;
+            case '-':
+                this.result = substract(a, b);
+                break;
+            case '*':
+                this.result = multiply(a, b);
+                break;
+            case '/':
+                this.result = divide(a, b);
+                break;
+            default: return null;
+        }
+    },
+    result: null,
 
+}
 const display = document.querySelector(".display");
 let displayNumber;
 const buttonsNodeL = document.querySelectorAll('.digits button');
+const operatorsNodel = document.querySelectorAll('.operators button')
+const resultButton = document.querySelector('.result');
+const clearButton = document.querySelector('.clear');
 
-buttonsNodeL.forEach(button => button.addEventListener('click', () => populateDisplay(button.textContent)
 
 
+buttonsNodeL.forEach(button => button.addEventListener('click', () => {
+    storeNumbers(button.textContent);
+    updateDisplay();
+}
 ));
 
+operatorsNodel.forEach(operator => operator.addEventListener('click', () => {
+    operationObj.operator = operator.textContent;
+    updateDisplay();
+}))
 
+resultButton.addEventListener('click', () => { 
+    operationObj.operate();
+    updateDisplay();
+    console.log(operationObj.result);
+})
+
+clearButton.addEventListener('click', ()=>{
+    clearObj();
+    updateDisplay();
+})
 
 
 function add(a, b){
@@ -31,32 +73,30 @@ function divide(a, b){
     return a / b;
 }
 
-function operate(a, b, operator){
-    switch(operator){
-        case '+':
-            add(a, b);
-            break;
-        case '-':
-            substract(a, b);
-            break;
-        case '*':
-            multiply(a, b);
-            break;
-        case '/':
-            divide(a, b);
-            break;
-    }
-}
 
-function populateDisplay(toBeDisplayed){
-    displayNumber = toBeDisplayed;
-    display.textContent = toBeDisplayed;
-    console.log(displayNumber);
+function updateDisplay(){
+    if (operationObj.result === null){
+    display.textContent = `${operationObj.a} ${operationObj.operator} ${operationObj.b}`;}
+    else{
+        display.textContent = operationObj.result;
+    }
 }
 
 
 function storeNumbers(pressed){
-    let a = aNumber;
-    let b = bNumber;
-    if (pressed 1=)
+    if (operationObj.operator == ''){
+        operationObj.a += pressed;
+    } else if(operationObj.operator != ''){
+        operationObj.b += pressed;
+    }
 }
+function clearObj(){
+    operationObj.a = '';
+    operationObj.b = '';
+    operationObj.operator = '';
+    operationObj.result = null;
+}
+//get entered digit
+//declare 2 variables for names
+
+console.log(operationObj)
